5a084793a8b76252c3094d5a1dbae08b986d49eb
renl <renl@umich.edu>
2015-02-15 12:32:02 -0500

    model built

--- SocialMediaScanner/SocialMediaScanner/urls.py
+++ SocialMediaScanner/SocialMediaScanner/urls.py
@@ -9,5 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', login),
     url(r'^login/', login),
+
 )
--- SocialMediaScanner/core/views.py
+++ SocialMediaScanner/core/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from models import Comment
 
 # Create your views here.
 # my name is ...
 def login(request):
+    if request.method == 'POST':
+        cname = request.POST['username']
+        print cname
+    return render(request, 'login.html')
+
+def index(request):
     return render(request, 'login.html')
