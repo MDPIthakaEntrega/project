1717feb47723b314d60fb1f5c1d8f42995e3983d
renl <renl@umich.edu>
2015-02-15 12:12:45 -0500

    model built

--- SocialMediaScanner/core/models.py
+++ SocialMediaScanner/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    company = models.ForeignKey(Company)
+    content = models.TextField()
