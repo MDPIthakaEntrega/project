f5600880b56ccd69a753daa1a3cc653af14f0694
renl <renl@umich.edu>
2015-03-23 19:08:43 -0400

    change a simplified user to a full user

--- SocialMediaScanner/core/models.py
+++ SocialMediaScanner/core/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from django.contrib.auth import models as auth_models
 # Create your models here.
-class Company(models.Model):
-    name = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
+class UserProfile(auth_models.AbstractBaseUser):
+    name = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    managerName = models.CharField(max_length=100, blank=True)
+    habitCode = models.IntegerField()
 
 
 class Comment(models.Model):
