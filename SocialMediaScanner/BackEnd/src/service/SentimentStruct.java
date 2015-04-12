c5bf85ea2b421d0f093b5ca54eb5e4f48c1e6cc3
Charlie Scott <cwscott@umich.edu>
2015-04-12 12:25:34 -0400

    Comments to sentimentStruct

--- SocialMediaScanner/BackEnd/src/service/SentimentStruct.java
+++ SocialMediaScanner/BackEnd/src/service/SentimentStruct.java
@@ -2,35 +2,64 @@ package service;
 
 public class SentimentStruct {
 
+	
+	/**
+	 * "positive", "negative", "neutral", or "mixed" to describe sentiment  
+	 */
 	private String sentiment_feeling;
 	private double sentiment_score;
 	
-	SentimentStruct() {
+	/**
+	 * default constructor;
+	 * sentiment defaults to neutral and 0.0;
+	 */
+	public SentimentStruct() {
 		sentiment_feeling = "neutral";
 		sentiment_score = 0.0;
 	}
 	
-	SentimentStruct(String sentiment_feeling_i, double sentiment_score_i) {
+	
+	/**
+	 * constructor;
+	 * @param sentiment_feeling_i
+	 * @param sentiment_score_i
+	 */
+	public SentimentStruct(String sentiment_feeling_i, double sentiment_score_i) {
 		
 		sentiment_feeling = sentiment_feeling_i;
 		sentiment_score = sentiment_score_i;
 	}
 	
-	
-	String getFeeling() {
+	/**
+	 * getter of the sentiment feeling;
+	 * @return
+	 */
+	public String getFeeling() {
 		return sentiment_feeling;
 	
 	}
 	
-	double getScore() {
+	/**
+	 * getter of the sentiment score;
+	 * @return
+	 */
+	public double getScore() {
 		return sentiment_score;
 	}
 	
-	void setFeeling(String sentiment_feeling_i) {
+	/**
+	 * setter of the sentiment feeling;
+	 * @return
+	 */
+	public void setFeeling(String sentiment_feeling_i) {
 		sentiment_feeling = sentiment_feeling_i;
 	}
 	
-	void setScore(double sentiment_score_i) {
+	/**
+	 * setter of the sentiment score;
+	 * @return
+	 */
+	public void setScore(double sentiment_score_i) {
 		sentiment_score = sentiment_score_i;
 	}
 }
