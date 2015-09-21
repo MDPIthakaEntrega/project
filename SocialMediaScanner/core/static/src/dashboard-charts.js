ab61e6404158afade9245f6bdabaf9a5d3c7f798
emichang <emichang@umich.edu>
2015-09-21 12:59:10 -0400

    charts working

--- SocialMediaScanner/core/static/src/dashboard-charts.js
+++ SocialMediaScanner/core/static/src/dashboard-charts.js
@@ -23,8 +23,12 @@ var SentimentPieChart = React.createClass({
     },
     componentDidMount: function() {
         $.plot($("#flot-piechart"), this.props.data, this.state.pie_options);
+        console.log(this.props.data);
     },
     render: function() {
+        if (this.props.init) {
+            $.plot($("#flot-piechart"), this.props.data, this.state.pie_options);
+        }
         return(
             <div id="flot-piechart" style={{"width":"500px", "height":"500px"}} />
         )
@@ -103,8 +107,56 @@ var TimeChart = React.createClass({
 
         // Add the Flot version string to the footer
         $("#footer").prepend("Flot " + $.plot.version + " &ndash; ");
+        console.log(this.props.data);
     },
     render: function () {
+        if (this.props.init) {
+            var plot = $.plot("#flot-timeseries", [{data: this.props.data}], this.state.time_options);
+
+        // Create the overview plot
+        var overview = $.plot("#overview", [{data: this.props.data}], this.state.time_options);
+        var tempdata = this.props.data;
+        var temp_options = this.state.time_options;
+
+        // now connect the two
+        $("#flot-timeseries").bind("plotselected", function (event, ranges) {
+
+            // clamp the zooming to prevent eternal zoom
+            if (ranges.xaxis.to - ranges.xaxis.from < 0.00001) {
+                ranges.xaxis.to = ranges.xaxis.from + 0.00001;
+            }
+
+            if (ranges.yaxis.to - ranges.yaxis.from < 0.00001) {
+                ranges.yaxis.to = ranges.yaxis.from + 0.00001;
+            }
+
+            // do the zooming
+            var temp = [];
+            for (i = 0; i < tempdata.length; i++) {
+                if (tempdata[i][0] > ranges.xaxis.from && tempdata[i][0]) {
+                    temp.push(tempdata[i]);
+                }
+            }
+            console.log(temp);
+            plot = $.plot("#flot-timeseries", [{data: temp}],
+                $.extend(true, {}, temp_options, {
+                    xaxis: {min: ranges.xaxis.from, max: ranges.xaxis.to},
+                    yaxis: {min: ranges.yaxis.from, max: ranges.yaxis.to}
+                })
+            );
+
+            // don't fire event on the overview to prevent eternal loop
+            overview.setSelection(ranges, true);
+        });
+
+        $("#overview").bind("plotselected", function (event, ranges) {
+            plot.setSelection(ranges);
+        });
+
+        // Add the Flot version string to the footer
+        $("#footer").prepend("Flot " + $.plot.version + " &ndash; ");
+        console.log(this.props.data);
+        }
         return (
             <div class="demo-container">
                 <div id="flot-timeseries" class="demo-placeholder" style={{"float":"left", "width":"650px", "height":"500px"}}></div>
@@ -126,7 +178,8 @@ var DashboardPlotApp = React.createClass({
             workable: false,
             positive: 0,
             negative: 0,
-            neutral: 0
+            neutral: 0,
+            initialized: false
         };
     },
     calculateAnalytics: function () {
@@ -135,6 +188,7 @@ var DashboardPlotApp = React.createClass({
             positive: 0,
             negative: 0
         });
+        console.log(this.state.numberOfData);
         var date_dict = {};
         if (this.state.workable) {
             for (i = 0; i < this.state.numberOfData; ++i) {
@@ -143,13 +197,13 @@ var DashboardPlotApp = React.createClass({
 
                 // Count Sentiments
                 if (type == 'negative') {
-                    this.setState({negative: this.state.negative++});
+                    this.setState({negative: this.state.negative+1});
                 }
                 else if (type == 'positive') {
-                    this.setState({positive: this.state.positive++});
+                    this.setState({positive: this.state.positive+1});
                 }
                 else if (type == 'neutral') {
-                    this.setState({neutral: this.state.neutral++});
+                    this.setState({neutral: this.state.neutral+1});
                 }
 
                 //Get Date counts
@@ -168,52 +222,64 @@ var DashboardPlotApp = React.createClass({
             }
         }
 
-        this.setState({
-            pie_data: [{label: "Positive", data: this.state.positive, color: "#4572A7"},
+        console.log("positive");
+        console.log(this.state.positive);
+        var pie_data = [{label: "Positive", data: this.state.positive, color: "#4572A7"},
                 {label: "Negative", data: this.state.negative, color: "#AA4643"},
-                {label: "Neutral", data: this.state.neutral, color: "#80699B"}]
-        });
+                {label: "Neutral", data: this.state.neutral, color: "#80699B"}];
 
+        var time_data = [];
         for (var key in date_dict) {
             if (date_dict.hasOwnProperty(key)) {
                 curr_date = key;
                 var count = date_dict[key];
-                var temp = this.state.time_data;
-                var new_time_data = temp.concat([curr_date, count]);
-                this.setState({time_data: new_time_data});
+                time_data.push([curr_date, count]);
             }
         }
+        console.log(time_data.length);
+        return [pie_data, time_data];
     },
     retrieveAllData: function() {
         $.ajax({
+            port: 3456,
             type: 'GET',
+            crossDomain: true,
+            //url: 'http://35.2.73.31:3456/search?company%20name=zingerman%27s&keyword=',
+            //url: 'http://localhost:3456/search?company%20name=zingerman%27s&keyword=',
             url: '/static/data.json',
-            xhrFields: {
-                withCredentials: false
-            },
             dataType: 'json',
             success: function (data) {
-                this.setState({allData: data['reviews']});
-                this.setState({numberOfData: this.state.allData.length});
-                this.setState({workable: true});
-                this.calculateAnalytics();
+                var tempData = data['reviews'];
+                this.setState({
+                                allData: tempData,
+                                numberOfData: tempData.length,
+                                workable: true
+                            });
+                var chart_data = this.calculateAnalytics();
+
+                this.setState ({
+                    pie_data: chart_data[0],
+                    time_data: chart_data[1],
+                    initialized: true
+                });
+                console.log(this.state.pie_data);
+                console.log(this.state.time_data.length);
             }.bind(this),
             error: function (xhr, status, err) {
                 console.error(xhr, status, err.toString());
             }.bind(this)
         });
-        console.log(this.state.allData);
     },
-    componentWillMount: function() {
+    componentDidMount: function() {
         this.retrieveAllData();
     },
     render: function() {
         return(
-            <div>
+            <div style={{padding: '20px'}}>
                 Sentiment Pie Chart
-                <SentimentPieChart data={this.state.pie_data}/>
+                <SentimentPieChart data={this.state.pie_data} init={this.state.initialized}/>
                 Number of Reviews by Month
-                <TimeChart data={this.state.time_data}/>
+                <TimeChart data={this.state.time_data} init={this.state.initialized}/>
             </div>
         );
     }
