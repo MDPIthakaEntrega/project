dbeb5167378a81b38521d798a3b13e992c1549b6
Yuke Liao <liaoyuke@umich.edu>
2015-09-27 15:45:53 -0400

    fixed GrabberTwitter imports issues

--- SocialMediaScanner/BackEnd/src/grabber/GrabberTwitter.java
+++ SocialMediaScanner/BackEnd/src/grabber/GrabberTwitter.java
@@ -4,6 +4,12 @@ import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import service.ResponseStruct;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
 
 /**
  * Twitter data source grabber, which must extends DataGrabberGeneric and
