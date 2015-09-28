e1038ebbb2fe50ef522125674e1b25f09631eaa2
kinglouisxviii <renl@umich.edu>
2015-09-28 13:30:15 -0400

    delete dead code

--- SocialMediaScanner/core/util/utilities.py
+++ SocialMediaScanner/core/util/utilities.py
@@ -2,35 +2,11 @@ from django.shortcuts import render
 
 __author__ = 'renl'
 from SocialMediaScanner.settings import BASE_DIR
-from dbHandler.cassandra_pull_reviews import cassandrareviewspuller
-from dbHandler.cassandra_select_reviews import CityGridReviewSelector
 import json
 
 
 def date_handler(obj):
     return obj.isoformat() if hasattr(obj, 'isoformat') else obj
-
-
-def pullAndInitializeNewReviews(company_name, username):
-    cassandrareviewspuller.pullReviews(company_name)
-    res = CityGridReviewSelector.selectReview(company_name)
-    res.sort(key=lambda x: x.review_date, reverse=True)
-    temp = res[0: 5]
-    print temp
-    new_reviews = []
-    for i in range(len(temp)):
-        dic = {}
-        dic['review_id'] = temp[i].review_id
-        dic['title'] = temp[i].review_title
-        dic['content'] = temp[i].review_text
-        dic['time'] = temp[i].review_date
-        dic['author'] = temp[i].review_author
-        new_reviews.append(dic)
-        finalizedData = {u'new_reviews': new_reviews}
-
-    with open(BASE_DIR + '/core/static/new_review_cookie/' + username + '_cookies.json', 'w+') as file:
-        file.write(json.dumps(finalizedData, default=date_handler))
-        file.close()
 
 
 def get_form_data(form):
