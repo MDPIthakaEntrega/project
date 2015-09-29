0c6c36e4a77839e2e41ba3c1c61377cc51bd48c5
kinglouisxviii <renl@umich.edu>
2015-09-29 16:33:16 -0400

    review card refine

--- SocialMediaScanner/core/static/src/dashboard-review-feeds.js
+++ SocialMediaScanner/core/static/src/dashboard-review-feeds.js
@@ -56,6 +56,7 @@ var ReviewFeed = React.createClass({
             url: '/static/search.json',
             dataType: 'json',
             success: function (data) {
+                console.log(data);
                 this.setState({
                     data: data
                 });
